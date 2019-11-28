3846b990a66ab7748f1c63900a4e1a7c3fcc8f77
Anders Fischer-Nielsen <andersfischern@me.com>
2019-11-28 12:04:03 +0100

    Move fixpoint finding to new file

--- analyzer.py
+++ analyzer.py
@@ -3,6 +3,7 @@ from datatypes import Analysis, CFGBranch, CFGNode, type_assignment, type_declar
 from pycparser import parse_file, c_parser, c_generator, c_ast
 from cfg import cfg as generator, cfg_nodes
 from cfg_generator import convert_to_cfg
+from fixpoint import find_fixpoint
 
 
 def generate_CFG (path:str): 
@@ -10,45 +11,6 @@ def generate_CFG (path:str):
     body = ast.ext[0].body
     cfg = convert_to_cfg(body)
     return cfg, ast
-
-def find_fixpoint(cfg, transfer_functions) -> {}:
-    def apply_transfer_functions(cfg, transfer_functions) -> {}:
-        def apply(node):
-            if (isinstance(node, CFGNode)):
-                matching_functions = transfer_functions.get(node.type)
-                if (matching_functions is None):
-                    return ["bottom"]
-
-                for t in matching_functions:
-                    res = t(node)
-                    if res is not None: 
-                        return [res]
-
-            elif (isinstance(node, CFGBranch)):
-                left = []
-                for l in (node.left):
-                    left.append(apply(l))
-                right = []
-                for l in (node.right):
-                    right.append(apply(l))
-                # TODO: Conjunction!
-                return ["CONJUNCTION"]
-
-        vector = []
-        for entry in cfg:
-            #TODO: Perform conjunction with the existing value during analysis. 
-            vector.extend(apply(entry))
-        return vector
-
-    # Recursively find the fixpoint
-    def rec_apply(previous: list): 
-        res = apply_transfer_functions(cfg, transfer_functions)
-        if res == previous:
-            return res 
-        else:
-            return rec_apply(res)
-    
-    return rec_apply (apply_transfer_functions(cfg, transfer_functions))
 
 def getAllExpressionsInProgram(cfg) -> list:
     return [""]
